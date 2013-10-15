46117f18876e1e67f3a317236725b0d9bdb124ee
Jay Klehr <jay@consolecity.com>
2013-10-15 16:27:55 -0600

    Adding new Collection pager adapter and tests
    Needed this paginator adapter when dealing with a collection that isn't
    a paged collection.  Added tests for both paginator adapters.

--- src/Doctrine1/Paginator/Adapter/Pager.php
+++ src/Doctrine1/Paginator/Adapter/Pager.php
@@ -3,6 +3,7 @@
 namespace Doctrine1\Paginator\Adapter;
 
 use Zend\Paginator\Adapter\AdapterInterface;
+use Doctrine_Pager;
 
 class Pager implements AdapterInterface
 {
@@ -11,7 +12,7 @@ class Pager implements AdapterInterface
     protected $results = array();
     protected $numResults;
 
-    public function __construct(\Doctrine_Pager $pager)
+    public function __construct(Doctrine_Pager $pager)
     {
         $this->pager = $pager;
     }
@@ -37,7 +38,12 @@ class Pager implements AdapterInterface
         if (is_null($this->numResults)) {
             // The executeCount method requires the diablomedia fork of doctrine1
             // https://github.com/diablomedia/doctrine1/
-            $this->pager->executeCount();
+            // Which will prevent double execution of the pager's query
+            if (method_exists($this->pager, 'executeCount')) {
+                $this->pager->executeCount();
+            } else {
+                $this->pager->execute();
+            }
             $this->numResults = $this->pager->getNumResults();
         }
 
