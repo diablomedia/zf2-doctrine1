f2051566c4e88f4d97c851eb67ae4dcfebeb6165
Jay Klehr <jay@diablomedia.com>
2018-02-14 12:32:27 -0700

    Removing reference to removed classmap file

--- Module.php
+++ Module.php
@@ -10,9 +10,6 @@ class Module implements AutoloaderProviderInterface
     public function getAutoloaderConfig()
     {
         return [
-            'Zend\Loader\ClassMapAutoloader' => [
-                __DIR__ . '/autoload_classmap.php',
-            ],
             'Zend\Loader\StandardAutoloader' => [
                 'namespaces' => [
                     // if we're in a namespace deeper than one level we need to fix the \ in the path
